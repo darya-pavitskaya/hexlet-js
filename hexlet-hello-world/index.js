e6bf2171e1f4a42ca5e0bbf2aa72f3066c91e255
Darya Pavitskaya <dpavitskaya@gmail.com>
2024-01-15 18:17:27 +0300

    test

--- hexlet-hello-world/index.js
+++ hexlet-hello-world/index.js
@@ -1,4 +1,6 @@
-const fn = (a, b) => {
-  const c = a + b;
-  return c;
-};
+import getBirthYear from "./src/index.js";
+
+console.log("Сколько вам лет?");
+const age = 18; // Каким-то способом получаем возраст пользователя, через терминал. Сейчас не важно как
+const birthYear = getBirthYear(age);
+console.log(`Ваш год рождения: ${birthYear}`);
